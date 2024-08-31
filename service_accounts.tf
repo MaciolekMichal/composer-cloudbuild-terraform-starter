61c64ad8aa3b2ab2b31af697c1c0f4ca66af6ee9
MaciolekMichal <macprivyo@gmail.com>
2024-08-31 16:34:34 +0200

    Update service accounts creation

--- service_accounts.tf
+++ service_accounts.tf
@@ -1,4 +1,4 @@
-resource "google_service_account" "initial_composer_sa" {
+resource "google_service_account" "composer_sa" {
   provider = google
   account_id   = "composer-sa"
   display_name = "Cloud Composer Service Account"
@@ -7,15 +7,13 @@ resource "google_service_account" "initial_composer_sa" {
 resource "google_project_iam_member" "composer_sa" {
   provider = google
   project  = var.project_id
-  member   = "serviceAccount:${google_service_account.initial_composer_sa.email}" 
+  member   = "serviceAccount:${google_service_account.composer_sa.email}" 
   role     = "roles/composer.worker"
-
-  depends_on = [google_service_account.initial_composer_sa]
 }
 
-# resource "google_service_account_iam_member" "composer_sa" {
-#   provider = google
-#   service_account_id = google_service_account.composer_sa.name
-#   role = "roles/composer.ServiceAgentV2Ext"
-#   member = "serviceAccount:service-${var.project_number}@cloudcomposer-accounts.iam.gserviceaccount.com"
-# }
+resource "google_service_account_iam_member" "composer_sa" {
+  provider = google
+  service_account_id = google_service_account.composer_sa.name
+  role = "roles/composer.ServiceAgentV2Ext"
+  member = "serviceAccount:service-${var.project_number}@cloudcomposer-accounts.iam.gserviceaccount.com"
+}
